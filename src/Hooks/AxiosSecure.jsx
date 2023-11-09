e5eb5e0f70d16db906fc784e64e4a9365c087286
Rafi <delwar2021bd@gmail.com>
2023-11-09 07:25:15 +0600

    deployment issue (changed api)

--- src/Hooks/AxiosSecure.jsx
+++ src/Hooks/AxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://b8a11-server-side-rafi00000-3wkyattj6-nothing-team-pr.vercel.app",
+  baseURL: "b8a11-server-side-rafi00000.vercel.app",
   withCredentials: true,
 });
 
